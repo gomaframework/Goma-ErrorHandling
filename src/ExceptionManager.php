52caa56c81cb4d5628c5640d26e6b1086bc4a741
Daniel Gruber <daniel@ibpg.eu>
2017-08-17 03:15:52 +0200

    - ErrorHandling

--- src/ExceptionManager.php
+++ src/ExceptionManager.php
@@ -239,23 +239,41 @@ class ExceptionManager
     public static function Goma_ErrorHandler($err_severity, $err_msg, $err_file, $err_line, $errcontext = null) {
         if (0 === error_reporting()) { return false;}
 
-        switch($err_severity)
-        {
-            case E_ERROR:               throw new ErrorException            ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_WARNING:             throw new WarningException          ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_PARSE:               throw new ParseException            ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_NOTICE:              throw new NoticeException           ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_CORE_ERROR:          throw new CoreErrorException        ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_CORE_WARNING:        throw new CoreWarningException      ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_COMPILE_ERROR:       throw new CompileErrorException     ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_COMPILE_WARNING:     throw new CoreWarningException      ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_USER_ERROR:          throw new UserErrorException        ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_USER_WARNING:        throw new UserWarningException      ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_USER_NOTICE:         throw new UserNoticeException       ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_STRICT:              throw new StrictException           ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_RECOVERABLE_ERROR:   throw new RecoverableErrorException ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_DEPRECATED:          throw new DeprecatedException       ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_USER_DEPRECATED:     throw new UserDeprecatedException   ($err_msg, 0, $err_severity, $err_file, $err_line);
+        try {
+            switch ($err_severity) {
+                case E_ERROR:
+                    throw new ErrorException            ($err_msg, 0, $err_severity, $err_file, $err_line);
+                case E_WARNING:
+                    throw new WarningException          ($err_msg, 0, $err_severity, $err_file, $err_line);
+                case E_PARSE:
+                    throw new ParseException            ($err_msg, 0, $err_severity, $err_file, $err_line);
+                case E_NOTICE:
+                    throw new NoticeException           ($err_msg, 0, $err_severity, $err_file, $err_line);
+                case E_CORE_ERROR:
+                    throw new CoreErrorException        ($err_msg, 0, $err_severity, $err_file, $err_line);
+                case E_CORE_WARNING:
+                    throw new CoreWarningException      ($err_msg, 0, $err_severity, $err_file, $err_line);
+                case E_COMPILE_ERROR:
+                    throw new CompileErrorException     ($err_msg, 0, $err_severity, $err_file, $err_line);
+                case E_COMPILE_WARNING:
+                    throw new CoreWarningException      ($err_msg, 0, $err_severity, $err_file, $err_line);
+                case E_USER_ERROR:
+                    throw new UserErrorException        ($err_msg, 0, $err_severity, $err_file, $err_line);
+                case E_USER_WARNING:
+                    throw new UserWarningException      ($err_msg, 0, $err_severity, $err_file, $err_line);
+                case E_USER_NOTICE:
+                    throw new UserNoticeException       ($err_msg, 0, $err_severity, $err_file, $err_line);
+                case E_STRICT:
+                    throw new StrictException           ($err_msg, 0, $err_severity, $err_file, $err_line);
+                case E_RECOVERABLE_ERROR:
+                    throw new RecoverableErrorException ($err_msg, 0, $err_severity, $err_file, $err_line);
+                case E_DEPRECATED:
+                    throw new DeprecatedException       ($err_msg, 0, $err_severity, $err_file, $err_line);
+                case E_USER_DEPRECATED:
+                    throw new UserDeprecatedException   ($err_msg, 0, $err_severity, $err_file, $err_line);
+            }
+        } catch (\Exception $e) {
+            self::Goma_ExceptionHandler($e);
         }
 
         // block PHP's internal Error-Handler
---
+++ tests/ExceptionManagerTest.php
@@ -0,0 +1,35 @@
+<?php
+defined("IN_GOMA") or die();
+use Goma\Error\ExceptionManager;
+use PHPUnit\Framework\TestCase;
+/**
+ * Tests for ExceptionManager.
+ *
+ * @package	goma/error
+ * @link 	http://goma-cms.org
+ * @license LGPL http://www.gnu.org/copyleft/lesser.html see 'license.txt'
+ * @author 	Goma-Team
+ */
+
+class ExceptionManagerTest extends GomaUnitTest
+{
+    /**
+     * throw ignorable exception should not interrupt.
+     */
+    public function testthrowIgnorableException() {
+        try {
+            throw new \Goma\Error\NoticeException();
+        } finally {
+            $this->assertTrue(true);
+        }
+    }
+
+    /**
+     * tests implicit php error throwing.
+     */
+    public function testPHPNotice() {
+        define("MOTICE_1", 1);
+        define("MOTICE_1", 2);
+        $this->assertTrue(true);
+    }
+}
